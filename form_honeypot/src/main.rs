e5664c72679c28f631553ea8b115a939eb0726dc
Matthew <matthew.roland56@gmail.com>
2024-03-05 19:42:05 -0600

    Finished multithreaded webserver minimum

--- form_honeypot/src/main.rs
+++ form_honeypot/src/main.rs
@@ -1,3 +1,50 @@
+use std:: {
+    fs, io::prelude::*, net:: { TcpListener, TcpStream }, thread::{self, Thread}, time::Duration 
+};
+use threadpool::ThreadPool;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming() {
+        let stream: std::net::TcpStream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        })
+
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    let get_request = b"GET / HTTP/1.1\r\n";
+
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = 
+        if buffer.starts_with(get_request) {
+            ("HTTP/1.1 200 OK", "index.html")
+        } else if buffer.starts_with(sleep) {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+        } else {
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        };
+
+    let content = fs::read_to_string(filename).unwrap();
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        content.len(),
+        content
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
